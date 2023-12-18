00828990d6dd92e9fe2050a9f92b30daf66a5e00
angelica19810223 <154108766+angelica19810223@users.noreply.github.com>
2023-12-17 22:15:06 -0500

    initial.commit

---
+++ login-register/php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+
+
+// if($conexion){
+//     echo 'Conectado exitosamente a la base de datos';
+// } else{
+//     echo 'no se ha podido concetar a la base de datos';
+// }
+
+
+?>
